35f8c144bf71ac7011fc9efae21c9def09b87ad9
ryo katsuma <katsuma@gmail.com>
2009-05-10 00:06:20 +0900

    自宅環境のconfig系をまとめてコミットしなおし
    .zshrc, .screenrc, .emacs.el, .vimrcなどコミット

---
+++ .emacs.el
@@ -0,0 +1,114 @@
+(when (eq window-system 'mac)
+  (add-hook 'window-setup-hook
+   (lambda ()
+;;              (setq mac-autohide-menubar-on-maximize t)
+    (set-frame-parameter nil 'fullscreen 'fullboth)
+    )))
+
+(defun mac-toggle-max-window ()
+  (interactive)
+  (if (frame-parameter nil 'fullscreen)
+      (set-frame-parameter nil 'fullscreen nil)
+    (set-frame-parameter nil 'fullscreen 'fullboth)))
+
+;; Carbon Emacsの設定で入れられた. メニューを隠したり．
+;(custom-set-variables
+ ;'(display-time-mode t)
+ ;'(tool-bar-mode nil)
+ ;'(transient-mark-mode t))
+;(custom-set-faces)
+
+;; Color
+(if window-system (progn
+   (set-background-color "Black")
+   (set-foreground-color "LightGray")
+   (set-cursor-color "Gray")
+   (set-frame-parameter nil 'alpha 80)
+   ))
+
+;; Macのキーバインドを使う。optionをメタキーにする。
+(mac-key-mode 1)
+(setq mac-option-modifier 'meta)
+
+;; タブキー
+(setq default-tab-width 4)
+(setq indent-line-function 'indent-relative-maybe)
+
+;; シフト + 矢印で範囲選択
+(setq pc-select-selection-keys-only t)
+(pc-selection-mode 1)
+
+;; 行数を表示
+ (setq line-number-mode t)
+
+;; ビープ音を消す
+(setq visible-bell t)
+(setq ring-bell-function 'ignore)
+
+;; スタートアップページを表示しない
+(setq inhibit-startup-message t)
+
+;; 対応する括弧を光らせる。
+(show-paren-mode 1)
+
+;; 編集行のハイライト
+(global-hl-line-mode)
+
+;; C-hをBSに割当
+(global-set-key "\C-h" 'delete-backward-char)
+
+;; elisp
+(setq load-path (cons "~/.emacs.d/elisp" load-path))
+
+;; install-elisp
+(require 'install-elisp)
+(setq install-elisp-repository-directory "~/.emacs.d/elisp")
+
+;; auto-compleyte-mode
+(require 'auto-complete)
+(global-auto-complete-mode t)
+
+
+
+;; ElScreenの有効化
+(require 'elscreen)
+;; PrefixキーをC-zに割り当て
+(if window-system
+ (define-key elscreen-map "\C-z" 'iconify-or-deiconify-frame)
+  (define-key elscreen-map "\C-z" 'suspend-emacs))
+
+
+;; linumを有効化
+(require 'linum)
+;; デフォルトでONにする
+(global-linum-mode 1)
+;; F5キーにON/OFFの切り替えを割り当てる
+(global-set-key [f5] 'linum-mode)
+;; 5桁とスペースの領域を割り当てる
+(setq linum-format "%5d ")
+
+
+
+;; jaspaceを有効化
+(require 'jaspace)
+;; 全角空白を表示させる
+(setq jaspace-alternate-jaspace-string "□")
+;; 改行記号を表示させる
+(setq jaspace-alternate-eol-string "↓\n")
+;; タブを表示
+(setq jaspace-highlight-tabs t)
+;; フック
+(add-hook 'text-mode-hook 'jaspace-mode)
+
+
+;; ri-emacs
+(setq ri-ruby-script "ri-emacs.rb")
+(autoload 'ri "ri-ruby.el" nil t)
+(add-hook 'ruby-mode-hook (lambda ()
+(local-set-key "\M-r" 'ri)
+(local-set-key "\M-c" 'ri-ruby-complete-symbol)
+;(local-set-key "\M-g" 'ri-ruby-show-args)
+))
+
+;; rcodetools
+;(require 'anything-rcodetools)
---
+++ .zshrc
@@ -0,0 +1,108 @@
+export LANG=ja_JP.UTF-8
+HISTFILE=$HOME/.zsh-history
+HISTSIZE=100000
+SAVEHIST=100000
+
+## alias
+alias l='ls -l'
+alias ll='ls -al'
+alias vi='vim'
+alias mysql='mysql5'
+
+## prompt
+local GREEN=$'%{\e[1;32m%}'
+local YELLOW=$'%{\e[1;33m%}'
+local BLUE=$'%{\e[1;34m%}'
+local DEFAULT=$'%{\e[1;m%}'
+PROMPT=$'\n'$GREEN'${USER}@${HOSTNAME} '$YELLOW'%~ '$'\n'$DEFAULT'%(!.#.$) '
+
+export PATH=/opt/local/sbin:/opt/local/bin:$PATH
+export JAVA_HOME=/Library/Java/Home
+
+## 補完機能の強化
+autoload -U compinit
+compinit
+
+source ~/.zsh_extend/cdd
+
+## コアダンプサイズを制限
+limit coredumpsize 102400
+
+## 出力の文字列末尾に改行コードが無い場合でも表示
+unsetopt promptcr
+
+## 色を使う
+setopt prompt_subst
+
+## ビープを鳴らさない
+setopt nobeep
+
+## 内部コマンド jobs の出力をデフォルトで jobs -l にする
+setopt long_list_jobs
+
+## 補完候補一覧でファイルの種別をマーク表示
+setopt list_types
+
+## サスペンド中のプロセスと同じコマンド名を実行した場合はリジューム
+setopt auto_resume
+
+## 補完候補を一覧表示
+setopt auto_list
+
+## 直前と同じコマンドをヒストリに追加しない
+setopt hist_ignore_dups
+
+## cd 時に自動で push
+setopt autopushd
+
+## 同じディレクトリを pushd しない
+setopt pushd_ignore_dups
+
+## ファイル名で #, ~, ^ の 3 文字を正規表現として扱う
+setopt extended_glob
+
+## TAB で順に補完候補を切り替える
+setopt auto_menu
+
+## zsh の開始, 終了時刻をヒストリファイルに書き込む
+setopt extended_history
+
+## =command を command のパス名に展開する
+setopt equals
+
+## --prefix=/usr などの = 以降も補完
+setopt magic_equal_subst
+
+## ヒストリを呼び出してから実行する間に一旦編集
+setopt hist_verify
+
+# ファイル名の展開で辞書順ではなく数値的にソート
+setopt numeric_glob_sort
+
+## 出力時8ビットを通す
+setopt print_eight_bit
+
+## ヒストリを共有
+setopt share_history
+
+## 補完候補のカーソル選択を有効に
+zstyle ':completion:*:default' menu select=1
+
+## 補完候補の色づけ
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+
+## ディレクトリ名だけで cd
+setopt auto_cd
+
+## カッコの対応などを自動的に補完
+setopt auto_param_keys
+
+## ディレクトリ名の補完で末尾の / を自動的に付加し、次の補完に備える
+setopt auto_param_slash
+
+## スペルチェック
+setopt correct
+
+function chpwd(){
+	_reg_pwd_screennum
+}
